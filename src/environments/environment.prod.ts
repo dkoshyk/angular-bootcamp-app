7aa04cb31142dfc8d7cecc56a8d71a988b45d9b0
koshyk <dimakoshyk@gmail.com>
2019-03-13 00:00:15 +0200

    [~] fix prod build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCZLb4LBZ6TMErhv6R5OwUbuOEj4sF4Uko",
+    authDomain: "try-angular-855bd.firebaseapp.com",
+    databaseURL: "https://try-angular-855bd.firebaseio.com",
+    projectId: "try-angular-855bd",
+    storageBucket: "try-angular-855bd.appspot.com",
+    messagingSenderId: "891411001887"
+  }
 };
